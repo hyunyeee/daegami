44dc36c4104eb99d92a2654ae39083e6bc5a449c
hyunyeee <5004ohy@naver.com>
2025-05-23 02:35:51 +0900

    refactor: 토글 변경시 confirm 후 진행

--- store/type.ts
+++ store/type.ts
@@ -1,5 +1,7 @@
 import { create } from "zustand";
 import { TypeKey } from "@/types";
+import { useChatStore } from "./chat";
+import { useRegionStore } from "./region";
 
 type State = {
   type: Partial<TypeKey>;
@@ -11,10 +13,15 @@ type Action = {
 
 const useTypeStore = create<State & Action>((set) => ({
   type: "period",
-  toggleType: () =>
-    set((state) => ({
-      type: state.type === "period" ? "location" : "period",
-    })),
+  toggleType: () => {
+    if (confirm("채팅 내역이 초기화 됩니다. 진행할까요?")) {
+      useChatStore.getState().clearMessages();
+      useRegionStore.getState().resetRegion();
+      set((state) => ({
+        type: state.type === "period" ? "region" : "period",
+      }));
+    }
+  },
 }));
 
 export { useTypeStore };

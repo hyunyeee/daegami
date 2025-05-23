f067f9f69974f094d5aa5809322d5252e6cf1e87
hyunyeee <5004ohy@naver.com>
2025-05-23 16:36:56 +0900

    feat: 예시 메세지 추가

--- src/components/chat/Intro.tsx
+++ src/components/chat/Intro.tsx
@@ -16,6 +16,22 @@ export const Intro = ({ type }: IntroProps) => {
         {getLabel(type)}
       </b>{" "}
       상담을 도와드리겠습니다.
+      {type === "region" ? (
+        <div className="text-zinc-500 italic">
+          <p>&lt; example &gt; </p>
+
+          <p>- 강북구 상권 분석해 줘 카페 하고 싶어</p>
+          <p>- 국밥집 차리고 싶어 어떻게 생각해?</p>
+          <p>- 망원동에서 수학학원 하려는데 어때?</p>
+        </div>
+      ) : (
+        <div className="text-zinc-500 italic">
+          <p>&lt; example &gt; </p>
+
+          <p>- 지금 창업하면 잘 될까?</p>
+          <p>- 내년에 창업하는 거 어때?</p>
+        </div>
+      )}
     </DaegamiChatBubble>
   );
 };
